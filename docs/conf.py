6627d9abab4e98bd1fc913094a4855266ee89647
Vladimir Prusakov <dzhedaj@gmail.com>
2023-11-21 17:42:43 +0200

    [fix] create_doc

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,9 @@
 For the full list of built-in configuration values, see the documentation:
 https://www.sphinx-doc.org/en/master/usage/configuration.html
 """
-from docs import create_doc
-
-create_doc.connectors()
+# from docs import create_doc
+#
+# create_doc.connectors()
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
