a98dc1e1e4729fb6bf2271c092e48903e7fee8cb
Vladimir Prusakov <dzhedaj@gmail.com>
2023-12-25 09:34:38 +0200

    test__version__tar

--- tests/test__package.py
+++ tests/test__package.py
@@ -9,22 +9,16 @@ ROOT = Path(__file__).parent.parent
 PYPROJECT_D = vdict.pyproject_d(ROOT)
 
 
-def test__version__readme():
-    """Version in README, URL."""
+def test__version__tar():
+    """Version in tar.gz."""
     expected = PYPROJECT_D["tool"]["poetry"]["version"]
     package = PYPROJECT_D["tool"]["poetry"]["name"].replace("_", "-")
-    # readme = PYPROJECT_D["tool"]["poetry"]["readme"]
-    # readme_text = Path.joinpath(ROOT, readme).read_text(encoding="utf-8")
-    url_toml = "pyproject.toml project.urls.DownloadURL"
-    url_text = PYPROJECT_D["tool"]["poetry"]["urls"]["Download URL"]
+    regex_tar = fr"{package}.+/(.+?)\.tar\.gz"
 
-    for source, text in [
-        # (readme, readme_text),
-        (url_toml, url_text),
-    ]:
-        regexes = [fr"{package}.+/(.+?)\.tar\.gz", fr"{package}@(.+?)$"]
-        versions = [v for s in regexes for v in re.findall(s, text, re.M)]
-        assert expected in versions, f"version {expected} not in {source}"
+    # pyproject.toml
+    text = PYPROJECT_D["tool"]["poetry"]["urls"]["Download URL"]
+    actual = vre.find1(regex_tar, text)
+    assert actual == expected
 
 
 def test__version__changelog():
